292bb2a13c56000e5f223ed87040faa861f59142
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-03 11:39:47 -0700

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,65 @@
 import React from "react";
 
-class TwitterMessage extends React.Component {
-  constructor() {
+{/* export default class TwitterMessage extends React.Component {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      counter: props.maxChars
+    };
   }
 
+  handleInputChange = (event) => {
+    event.persist()
+    this.setState(previousState => {
+      return {
+        counter: previousState.counter - event.target.value.length
+      }
+    })
+  };
+
   render() {
+    // console.log(this.state.maxChars)
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleInputChange}/>
+        <p>{this.state.counter} characters left</p>
+      </div>
+    );
+  }
+} */}
+
+// export default TwitterMessage;
+ 
+// maxChars = 280
+// Make this a controlled component by adding the attributes to the <input> element.
+//// √ add maxChars to state
+//// √ add value=this.state.maxChars
+
+export default class TwitterMessage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  render() {
+    let charNumber = this.props.maxChars - this.state.message.length;
+    return (
+      <div>
+        <strong>Your message:</strong>
+        <input type="text" onChange={this.handleChange} value={this.state.message}/>
+        {charNumber}
       </div>
     );
   }
 }
-
-export default TwitterMessage;
